94ca847fac2ed716183892ff3dd4bee318d641e2
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-15 07:57:28 +0330

    feat(layouts): update navbar with profile dropdown and improve dark mode toggle
    - Changed "register" icon in navbar to "user" for better representation.
    - Added a profile dropdown with logout functionality for authenticated users.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <flux:navbar.item icon="calendar" href="#">Calendar</flux:navbar.item>
         <flux:navbar.item x-data x-on:click="$flux.dark = ! $flux.dark" icon="moon" variant="subtle" aria-label="Toggle dark mode" />
         <flux:navbar.item icon="arrow-left-start-on-rectangle" href="{{ route('register') }}">{{ __('common.login') }}</flux:navbar.item>
-        <flux:navbar.item icon="user-plus" href="{{ route('login') }}">{{ __('common.register') }}</flux:navbar.item>
+        <flux:navbar.item icon="user" href="{{ route('login') }}">{{ __('common.register') }}</flux:navbar.item>
     </flux:navbar>
     @auth
         <flux:dropdown position="top" align="start">
@@ -38,6 +38,16 @@
             </flux:menu>
         </flux:dropdown>
     @endauth
+
+        <flux:dropdown position="top" align="start">
+            <flux:profile />
+            <flux:menu>
+                @auth
+                    <flux:menu.item href="{{ route('logout') }}" icon="arrow-right-start-on-rectangle">{{ __('common.logout') }}</flux:menu.item>
+                @endauth
+            </flux:menu>
+        </flux:dropdown>
+
 
 </flux:header>
 <flux:sidebar sticky collapsible="mobile"
