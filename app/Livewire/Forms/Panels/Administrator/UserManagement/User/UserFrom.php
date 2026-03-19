45a03ccd83444ffc1c984bb3d8e1644287126673
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-19 11:51:23 +0330

    add livewire UserForm

---
+++ app/Livewire/Forms/Panels/Administrator/UserManagement/User/UserFrom.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Forms\Panels\Administrator\UserManagement\User;
+
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class UserFrom extends Form
+{
+    //
+}
