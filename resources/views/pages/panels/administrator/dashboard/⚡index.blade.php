d9d5e5d4430920cd80a4f33c3f16bae48d5363f6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-28 09:54:30 +0330

    set dashboard title using localized `common.dashboard` translation slot

--- resources/views/pages/panels/administrator/dashboard/⚡index.blade.php
+++ resources/views/pages/panels/administrator/dashboard/⚡index.blade.php
@@ -9,6 +9,11 @@ new #[Layout('layouts.panels.administrator')] class extends Component
 };
 ?>
 
+<x-slot name="title">
+    {{ __('common.dashboard') }}
+</x-slot>
+
+
 <flux:main>
     {{-- Do what you can, with what you have, where you are. - Theodore Roosevelt --}}
 </flux:main>
