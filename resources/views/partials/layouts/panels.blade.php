08f415ee77a55dba4c66a508a3303542e8f08c77
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-28 18:02:18 +0330

    add administrator permission access.

--- resources/views/partials/layouts/panels.blade.php
+++ resources/views/partials/layouts/panels.blade.php
@@ -1,5 +1,8 @@
 <flux:sidebar.nav>
     <flux:sidebar.item icon="cog-6-tooth" href="{{ route('settings.profile-information') }}">{{ __('common.setting_panel') }}</flux:sidebar.item>
     <flux:sidebar.item icon="user" href="#">{{ __('common.user_panel') }}</flux:sidebar.item>
-    <flux:sidebar.item icon="shield-user" href="#">{{ __('common.administrator_panel') }}</flux:sidebar.item>
+    @can('administrator_access')
+        <flux:sidebar.item icon="shield-user" href="#">{{ __('common.administrator_panel') }}</flux:sidebar.item>
+    @endcan
+
 </flux:sidebar.nav>
