3cc0062ddf7dc2ce416f96362c6d8f085847bcd4
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-28 20:14:58 +0330

    update sidebar items to include dynamic active state and remove @blaze directive from search layout blade

--- resources/views/partials/layouts/panels.blade.php
+++ resources/views/partials/layouts/panels.blade.php
@@ -1,6 +1,6 @@
 <flux:sidebar.nav>
-    <flux:sidebar.item icon="user" href="{{ route('panels.user.dashboard.index') }}" wire:navigate>{{ __('common.user_panel') }}</flux:sidebar.item>
+    <flux:sidebar.item icon="user" href="{{ route('panels.user.dashboard.index') }}" wire:navigate :current="request()->routeIs('panels.user.*')">{{ __('common.user_panel') }}</flux:sidebar.item>
     @can('administrator_access')
-        <flux:sidebar.item icon="shield-user" href="{{ route('panels.administrator.user-management.user.index') }}" wire:navigate>{{ __('common.administrator_panel') }}</flux:sidebar.item>
+        <flux:sidebar.item icon="shield-user" href="{{ route('panels.administrator.user-management.user.index') }}" wire:navigate :current="request()->routeIs('panels.administrator.*')">{{ __('common.administrator_panel') }}</flux:sidebar.item>
     @endcan
 </flux:sidebar.nav>
