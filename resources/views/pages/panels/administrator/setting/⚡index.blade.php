c233000f75e79ee55377877db449d3c032f139ca
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-29 18:54:08 +0330

    remove laravel-settings package and related settings classes; update administrator settings page and composer dependencies

--- resources/views/pages/panels/administrator/setting/⚡index.blade.php
+++ resources/views/pages/panels/administrator/setting/⚡index.blade.php
@@ -5,7 +5,22 @@ use Livewire\Attributes\Layout;
 
 new #[Layout('layouts.panels.administrator')] class extends Component
 {
-    //
+    public string $app_name = "";
+
+    public function mount(App\Settings\GeneralSettings $generalSettings)
+    {
+        $this->app_name = $generalSettings->app_name;
+    }
+
+    public function save(App\Settings\GeneralSettings $generalSettings)
+    {
+        $generalSettings->app_name = "PCShiraz";
+        $generalSettings->app_short_name = "PC";
+        $generalSettings->app_domain = "PCShiraz.ir";
+        $generalSettings->app_description = "PCShiraz";
+        $generalSettings->save();
+        $this->app_name = $generalSettings->app_name;
+    }
 };
 ?>
 
@@ -15,5 +30,5 @@ new #[Layout('layouts.panels.administrator')] class extends Component
 
 
 <flux:main>
-    {{-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama --}}
+        <flux:button wire:click="save()">Save</flux:button>
 </flux:main>
