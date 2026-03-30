f56f05613430bdb06151482c8bf7f836e49c53b7
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-03-30 09:42:08 +0330

    extend GeneralSettings with additional app configuration fields; update setting view

--- resources/views/components/panels/administrator/setting/⚡general.blade.php
+++ resources/views/components/panels/administrator/setting/⚡general.blade.php
@@ -4,7 +4,10 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public string $general_app_name = '';
+    public string $general_app_short_name = '';
+    public string $general_app_short_description = '';
+    public string $general_app_logo = '';
 };
 ?>
 
