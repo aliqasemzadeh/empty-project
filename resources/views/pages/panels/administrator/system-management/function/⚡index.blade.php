5ee466f2846b6b884310a5d79160e36ba7a0b507
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-08 11:48:24 +0330

    add system management function view for administrator panel

---
+++ resources/views/pages/panels/administrator/system-management/function/⚡index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+new #[Layout('layouts.panels.administrator')] class extends Component
+{
+    //
+};
+?>
+
+<flux:main>
+    {{-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant --}}
+</flux:main>
