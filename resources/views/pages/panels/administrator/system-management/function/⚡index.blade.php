76a8f5ba0f9c135f8a7719145137d2d1076a06dc
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-04-08 15:56:13 +0330

    add title, subheading, and separator to system management functions view; update Farsi translation for functions description

--- resources/views/pages/panels/administrator/system-management/function/⚡index.blade.php
+++ resources/views/pages/panels/administrator/system-management/function/⚡index.blade.php
@@ -8,7 +8,20 @@ new #[Layout('layouts.panels.administrator')] class extends Component
     //
 };
 ?>
+<x-slot name="title">
+    {{ __('common.functions') }} - {{ __('common.administrator_panel') }} - {{ config('common.name') }}
+</x-slot>
+
 
 <flux:main>
-    {{-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant --}}
+    <div class="relative mb-6 w-full">
+        <div class="flex items-center justify-between">
+            <div>
+                <flux:heading size="xl" level="1">{{ __('common.functions') }}</flux:heading>
+                <flux:subheading size="lg" class="mb-6">{{ __('common.functions_description') }}</flux:subheading>
+            </div>
+        </div>
+
+        <flux:separator variant="subtle" />
+    </div>
 </flux:main>
